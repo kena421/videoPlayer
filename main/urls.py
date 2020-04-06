272e4b6eac97e16370291b43e64ffcbdc40a7400
amanloc1 <amanloc1@gmail.com>
2020-04-06 23:54:41 +0530

    ghjj

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'), 
+    # path('', views.index, name='index'), 
+    path('', views.model_form_upload, name='index'), 
+
           
 ]
